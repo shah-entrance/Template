4ae1f76662a9497675aa517d69d9aa34190d1601
Md Shahbaz Alam <40912165+shah-entrance@users.noreply.github.com>
2020-09-16 10:40:31 +0530

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,10 @@
+vector<string> split(string str,char delimeter){
+    stringstream ss(str);
+    string s;
+    vector<string> out;
+    while(getline(ss,s,delimeter)){
+        if(s.length()!=0)
+            out.push_back(s);
+    }
+    return out;
+}
